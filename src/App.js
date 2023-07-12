57b2eb13f1273d08f250f883a08e6e36b9668b59
Yuliia2991 <yuliiakurian@gmail.com>
2023-07-12 18:33:20 +0300

    Add a component Statistics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Profile from './components/Profile/Profile.jsx';
+import Statistics from './components/Statistics/Statistics.jsx';
 import user from './user.json';
+import data from './data.json';
 
 export default function App() {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 }
